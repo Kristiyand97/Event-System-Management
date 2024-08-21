e406d7c40b1673dffc2ee24db69b7c67bba31558
Kristiyand97 <kristiyand97@gmail.com>
2024-08-21 18:49:20 +0300

    Tests for users app views have been added.

--- event_management/users/tests/test_users.py
+++ event_management/users/tests/test_users.py
@@ -2,10 +2,6 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
-from django.utils.http import urlencode
-
-from events.models import Event
-from tickets.models import Ticket
 
 User = get_user_model()
 
